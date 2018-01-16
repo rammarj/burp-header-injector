5339ac6e74b6ba9961f4a02ed30dbc56d0c5ac31
JoaquinRMtz <JoaquinRMtz@users.noreply.github.com>
2018-01-16 02:10:27 -0600

    uploaded

---
+++ src/burp/BurpExtender.java
@@ -0,0 +1,73 @@
+package burp;
+
+import burp.userinterface.UInterface;
+import java.awt.Component;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Joaquin R. Martinez
+ */
+public class BurpExtender implements IBurpExtender, IHttpListener {
+
+    private IBurpExtenderCallbacks ibec;
+    private UInterface uInterface;
+    private IExtensionHelpers helpers;
+
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks ibec) {
+        this.ibec = ibec;
+        helpers = ibec.getHelpers();
+        uInterface = new UInterface(this.ibec);
+        ibec.registerHttpListener(this);
+        ibec.addSuiteTab(new ITab() {
+            @Override
+            public String getTabCaption() {
+                return "Header injector";
+            }
+
+            @Override
+            public Component getUiComponent() {
+                return uInterface;
+            }
+        });
+    }
+
+    @Override
+    public void processHttpMessage(int flag, boolean isRequest, IHttpRequestResponse message) {
+        if (!isRequest&&(IBurpExtenderCallbacks.TOOL_PROXY == flag||IBurpExtenderCallbacks.TOOL_SPIDER == flag)
+                && (this.ibec.isInScope(message.getUrl()))) {
+            if (this.uInterface.alreadyExists(message)) {
+                return;
+            }
+            IRequestInfo req = this.helpers.analyzeRequest(message);
+            List<String> headers = req.getHeaders();
+            for (int i = headers.size() - 1; i >= 0; i--) {
+                String header = (String) headers.get(i);
+                if (header.startsWith("Host:")) {
+                    header = header + ".burp.header.injector.host";
+                    headers.remove(i);
+                    headers.add(i, header);
+                }
+                if (header.startsWith("Origin")) {
+                    headers.remove(i);
+                }
+//                if (get.startsWith("Referer")) {
+//                    headers.remove(i);
+//                }
+            }
+            headers.add("X-Forwarded-For: burp.header.injector.xff");
+            headers.add("X-Forwarded-Host: burp.header.injector.xfh");
+//            headers.add("Origin: burp.header.injector.origin");
+//            headers.add("Referer: burp.header.injector.referer");
+            byte[] newMsg = helpers.buildHttpMessage(headers, ("post".equals(req.getMethod().toLowerCase()))?Arrays.copyOfRange(message.getRequest(), req.getBodyOffset(), message.getRequest().length):null);   
+            IHttpRequestResponse newResponse = this.ibec.makeHttpRequest(message.getHttpService(), newMsg);
+            byte[] response = newResponse.getResponse();
+            if (this.helpers.indexOf(response, "burp.header.injector".getBytes(), false, 0, response.length - 1) != -1) {
+                this.uInterface.sendToTable(newResponse);
+            }
+        }
+    }
+
+}
